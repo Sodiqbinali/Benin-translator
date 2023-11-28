8a96febde0cdaafab03d001285eec418ec55dab0
sodiqbinaliu <sodiboy4jamb@gmail.com>
2023-11-28 09:55:25 +0100

    icon to swap language and text

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ toText = document.querySelector('.to-text'),
 btn = document.querySelector('button'),
 selectTag = document.querySelectorAll("select"),
 radio1 = document.querySelector('#eng');
+
+icon.addEventListener('click', () => {
+    let tempText = fromText.value;
+      fromText.value = toText.value;
+      toText.value = tempText;
+      selectTag[0].value = selectTag[1].value;
+  
+    if(langOpt1.display == 'none'){
+      langOpt1.display = 'flex';
+      biniLabel1.display = 'none';
+      langOpt2.display = 'none';
+      biniLabel2.display = 'flex';
+    }
+    else {
+      langOpt1.display = 'none';
+      biniLabel1.display = 'flex';
+      langOpt2.display = 'flex';
+      biniLabel2.display = 'none';
+    }
+  })
